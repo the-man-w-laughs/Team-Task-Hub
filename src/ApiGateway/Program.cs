0c7b8437c2579f8ed8473e07abdcbbf7a4686efe
Nazar Masukov <nazarmasukov@gmail.com>
2023-10-29 19:50:56 +0300

    Add routes to ocelot.json

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,6 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .AddJsonFile(builder.Configuration["Ocelot:JsonFile"], optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseSwaggerForOcelotUI(opt =>
+{
+    opt.PathToSwaggerGenerator = app.Configuration["Ocelot:PathToSwaggerGen"];
+});
+
+app.UseWebSockets();
+await app.UseOcelot();
 
 app.Run();
