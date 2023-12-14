bbd97c6422b558d991aaa182dba5e3746cb9dbe4
Nazar Masukov <nazarmasukov@gmail.com>
2023-12-14 14:52:20 +0300

    Add update and delete buttons to comment-section.

--- client/src/app/components/comment-section/comment-section.component.ts
+++ client/src/app/components/comment-section/comment-section.component.ts
@@ -15,16 +15,56 @@ export class CommentSectionComponent {
   author: string = '';
   content: string = '';
   comments: Comment[] = [];
+  editingCommentIndex: number | null = null;
 
   constructor() {
-    for (let i = 0; i < 100; i++) {
-      this.addComment(`User${i + 1}`, `This is comment ${i + 1}.`);
+    for (let i = 0; i < 10; i++) {
+      const uniqueAuthor = `User${i + 1}`;
+      const uniqueContent = `This is comment ${i + 1}.`;
+      this.author = uniqueAuthor;
+      this.content = uniqueContent;
+      this.addComment();
     }
   }
 
-  addComment(author: string, content: string) {
-    if (author && content) {
-      this.comments.push(new Comment(author, content, new Date()));
+  updateComment() {
+    if (this.content && this.editingCommentIndex !== null) {
+      this.comments[this.editingCommentIndex].content = this.content;
+      this.editingCommentIndex = null;
+      this.resetForm();
     }
+  }
+
+  addComment() {
+    if (this.author && this.content) {
+      if (this.editingCommentIndex !== null) {
+        this.updateComment();
+      } else {
+        this.comments.push(new Comment(this.author, this.content, new Date()));
+      }
+      this.resetForm();
+    }
+  }
+
+  deleteComment(index: number) {
+    if (index >= 0 && index < this.comments.length) {
+      this.comments.splice(index, 1);
+    }
+  }
+
+  startEditing(index: number) {
+    this.editingCommentIndex = index;
+    this.author = this.comments[index].author;
+    this.content = this.comments[index].content;
+  }
+
+  cancelEditing() {
+    this.editingCommentIndex = null;
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.author = '';
+    this.content = '';
   }
 }
